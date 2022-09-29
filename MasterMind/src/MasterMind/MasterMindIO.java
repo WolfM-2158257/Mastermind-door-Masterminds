2b8ed1c50a17bcba2f3f6122c52e5129da580e9b
WolfM-2158257 <wolf.moyaers@student.uhasselt.be>
2022-09-29 13:42:46 +0200

    man

--- MasterMind/src/MasterMind/MasterMindIO.java
+++ MasterMind/src/MasterMind/MasterMindIO.java
@@ -84,5 +84,6 @@ public class MasterMindIO {
     public static void clearConsole(){
         System.out.print("\033[H\033[2J");
         System.out.flush();
+        System.out.print("\f");
     }
 }
