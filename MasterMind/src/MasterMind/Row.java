32d75541c96d96ed3fd28788d204a32e16322bf1
SenneW-2158088 <91157018+SenneW-2158088@users.noreply.github.com>
2022-09-29 12:16:22 +0200

    Update Row.java

--- MasterMind/src/MasterMind/Row.java
+++ MasterMind/src/MasterMind/Row.java
@@ -1,5 +1,32 @@
 package MasterMind;
 
 public class Row {
-    // TODO: CREATE CLASS
+    private Code m_code;
+    private int m_pins_red;
+    private int m_pins_white;
+
+    public void Row(){
+        m_pins_red = 0; m_pins_white = 0;
+    }
+
+    /**
+    * Returns the row as a string
+    * @out: String
+    **/
+    public String GetRowAsString(){
+        return this.m_code.GetCodeAsString() + String.format("%dW, %dR", this.m_pins_white, this.m_pins_red);
+    }
+
+    /**
+     * Add a code to the row
+     * @Param: User input code
+     * @pre: Code is filled
+     **/
+    public void AddCode(Code code){
+        this.m_code = code;
+    }
+
+
+
+
 }
