2e31777d1567661aabf8276f323131faae8b04b4
LorrensP-2158466 <lorrens.pantelis@student.uhasselt.be>
2022-09-29 12:47:01 +0200

    functionality added

--- MasterMind/src/MasterMind/Code.java
+++ MasterMind/src/MasterMind/Code.java
@@ -1,7 +1,39 @@
 package MasterMind;
 
+
 public class Code {
     private int[] m_codes;
-    private int m_length;
+
+
+    public Code(String s){
+        String[] invCodes = s.split(" ");
+        m_codes = new int[invCodes.length];
+        int i = 0;
+        for (String c: invCodes) m_codes[i++] = Integer.parseInt(c);
+    }
+    public Code(int[] c){
+        m_codes = c;
+    }
+    /**
+     * Returns codeBlock at position pos
+     * @param pos
+     * @return int
+     *
+     * @pre 0 < pos && pos < Code.length()
+     * @post integer
+     */
+    public int getBlock(int pos){
+        return m_codes[pos];
+    }
+
+    /** Returns the length of code
+     *
+     * @return length of Code
+     */
+    public int getLength(){
+        return m_codes.length;
+    }
+
+
 
 }
