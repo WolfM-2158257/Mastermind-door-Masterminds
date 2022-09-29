7e260d83d021578e05bf25d527b1ceaa34df4c85
LorrensP-2158466 <lorrens.pantelis@student.uhasselt.be>
2022-09-29 11:01:32 +0200

    reading input

--- MasterMind/src/MasterMind/MasterMind.java
+++ MasterMind/src/MasterMind/MasterMind.java
@@ -3,11 +3,12 @@ package MasterMind;
 
 import java.awt.*;
 import java.io.Console;
-import java.util.Scanner;
+import java.util.*;
 
 public class MasterMind {
     private int m_amountColours;
-    private Scanner m_scan = new Scanner(System.in);
+    private final Scanner m_scan = new Scanner(System.in);
+    // private Code m_code;
 
     public int COLS; // len of code
     public int ROWS; // number of try's
@@ -19,8 +20,28 @@ public class MasterMind {
     }
 
     private void start(){
-        System.out.print("How many codebreaker tries: ");
-        int input = m_scan.nextInt();
-        System.out.println(input);
+        this.init();
+    }
+
+    private void init(){
+        ROWS = getIntInput("How many codebreaker tries? ");
+        COLS = getIntInput("How long can the code be? ");
+        m_amountColours = getIntInput("How many colours? ");
+    }
+
+    private int getIntInput(String s){
+        int ret = 0;
+        boolean success = false;
+        while (!success){
+            try{
+                System.out.print(s);
+                ret = m_scan.nextInt();
+                success = true;
+            } catch(InputMismatchException e){
+                System.out.println("You gave wrong input, must be integer. Thank you!");
+                m_scan.next();
+            }
+        }
+        return ret;
     }
 }
