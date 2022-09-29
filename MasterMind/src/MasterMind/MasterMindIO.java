f550a3a201606eb720899aaeb2bf0f9b2fbb1b67
LorrensP-2158466 <lorrens.pantelis@student.uhasselt.be>
2022-09-29 13:37:15 +0200

    read code from code breaker

--- MasterMind/src/MasterMind/MasterMindIO.java
+++ MasterMind/src/MasterMind/MasterMindIO.java
@@ -18,15 +18,15 @@ public class MasterMindIO {
      * Based on question s you can ask input from players.
      * Method handles non integer inputs on its own.
      *
-     * @param s Question to ask game player
+     * @param message Question to ask game player
      * @return answer of game player
      */
-    public int getIntInput(String s){
+    public int getIntInput(String message){
         int ret = 0;
         boolean success = false;
         while (!success){
             try{
-                System.out.print(s);
+                System.out.print(message);
                 ret = m_scan.nextInt();
                 success = true;
             } catch(Exception e){
@@ -37,9 +37,26 @@ public class MasterMindIO {
         return ret;
     }
 
+    public int[] getCode(String message, int max_len, int colourAmount){
+        int[] ret = new int[max_len];
+        System.out.println(message);
+        for (int i = 0; i < max_len; i++) {
+            boolean success = false;
+            int input = 0;
+            while (!success){
+                input = getIntInput("CodeBlock " + (i + 1) + ": ");
+                if (input < colourAmount) success = true;
+                else System.out.println("CodeBlock must be less than the amount of colours");
+            }
+            ret[i] = input;
+        }
+
+        return ret;
+    }
+
     public void printBoard(MasterMind mm){
         printPlayerScores(mm.getScorePlayer1(), mm.getScorePlayer2());
-        printRows(mm.getCodeLength(), mm.getBoard());
+        printRows(mm.COLS, mm.getBoard());
     }
 
     private void printRows(int codeLength, ArrayList<Row> board){
@@ -62,5 +79,10 @@ public class MasterMindIO {
         for (int i = 0; i < times; i++)
             ret += s;
         return ret;
+    }
+
+    public static void clearConsole(){
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
     }
 }
